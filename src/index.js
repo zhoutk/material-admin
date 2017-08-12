30190d3cdee22c63ceec34da0ac36632b3ca4a6a
zhoutk <zhoutk@189.cn>
2017-08-13 01:05:01 +0800

    init, BasicExample.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom'
+import BasicExample from './components/BasicExample';
+
+render(
+        <BasicExample/>,
+    document.getElementById('root')
+);

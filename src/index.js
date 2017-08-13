8ca2cb7661f946c81ace4b992bfe0642e3936e0c
zhoutk <zhoutk@189.cn>
2017-08-13 14:11:50 +0800

    react-router-redux to ver5.0, because router log not support immutable. add __REDUX_DEVTOOLS_EXTENSION__

--- src/index.js
+++ src/index.js
@@ -1,47 +1,69 @@
 import React from 'react';
 import {render} from 'react-dom';
 import { Provider } from 'react-redux'
-import { combineReducers, routerReducer, stateTransformer } from 'redux-seamless-immutable'
-import { createStore, applyMiddleware } from 'redux'
+import { stateTransformer } from 'redux-seamless-immutable'
+import { createStore, applyMiddleware,compose, combineReducers } from 'redux'
 import { createLogger } from 'redux-logger'
-import { routerMiddleware } from 'react-router-redux'
+import { ConnectedRouter, routerMiddleware } from 'react-router-redux'
+import rootReducer from './reducer';
 import App from './containers/App';
 import Home from './components/Home';
 import Stuff from './components/Stuff';
 import Contact from './components/Contact';
 import './index.css';
-import {BrowserRouter as Router,Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
+import createSagaMiddleware from 'redux-saga';
 
+const sagaMiddleware = createSagaMiddleware()
 const history = createHistory();
-const middleware = routerMiddleware(history)
-
-const rootReducer = combineReducers({
-    routing: routerReducer
-});
-
+const historyMiddleware = routerMiddleware(history);
 const loggerMiddleware = createLogger({
     collapsed:true,
     stateTransformer: stateTransformer
 });
 
-const store = createStore(
-    rootReducer,
-    applyMiddleware(
-        loggerMiddleware,
-        middleware
-    )
-);
+let store;
+
+if(process.env.NODE_ENV == "development") {
+    if (!(window.__REDUX_DEVTOOLS_EXTENSION__ || window.__REDUX_DEVTOOLS_EXTENSION__)) {
+        store = createStore(
+            rootReducer,
+            applyMiddleware(
+                loggerMiddleware,
+                historyMiddleware,
+                sagaMiddleware
+            )
+        );
+    } else {
+        store = createStore(
+            rootReducer,
+            compose(applyMiddleware(
+                loggerMiddleware,
+                historyMiddleware,
+                sagaMiddleware
+            ), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+        );
+    }
+}else{
+    store = createStore(
+        rootReducer,
+        applyMiddleware(
+            historyMiddleware,
+            sagaMiddleware
+        )
+    );
+}
 
 render(
     <Provider store={store}>
-        <Router history={history}>
+        <ConnectedRouter history={history}>
             <App>
                 <Route exact path="/" component={Home} />
                 <Route path="/stuff" component={Stuff} />
                 <Route path="/contact" component={Contact} />
             </App>
-        </Router>
+        </ConnectedRouter>
     </Provider>,
     document.getElementById('root')
 );
---
+++ src/reducer/index.js
@@ -0,0 +1,5 @@
+import { combineReducers, routerReducer } from 'redux-seamless-immutable'
+
+export default combineReducers({
+    routing: routerReducer
+});

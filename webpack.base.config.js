03696aef01dff1de9347f242d8512aa501182f0a
zhoutk <zhoutk@189.cn>
2017-08-13 09:31:50 +0800

    autoprefixer-loader -> postcss-loader.

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -46,7 +46,7 @@ const config = {
                         }
                     },
                         {
-                            loader: 'autoprefixer-loader',
+                            loader: 'postcss-loader',
                             options:{
                                 browsers:'last 2 version'
                             }
